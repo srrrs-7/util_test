984bdb74c694931fec27c230948ba820165c219d
sRRRs <srrrs4510@gmail.com>
2024-05-21 23:31:14 +0900

    [db sh] mysql connect

---
+++ database/util/connect.sh
@@ -0,0 +1,10 @@
+ #!/bin/sh
+ 
+ FILE=./data.txt
+ 
+ while read LINE
+ do
+     mysql --defaults-extra-file=./access.cnf -D test -e "select table_name from information_schema.tables;" >> "./data.txt"
+ done < $FILE
+ 
+ echo "done"
