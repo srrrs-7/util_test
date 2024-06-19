36dd6623624140ba2a55caacc2a9cd9cd593ca46
sRRRs <srrrs4510@gmail.com>
2024-06-19 23:25:02 +0900

    [rust] experiment

--- rust/dev/src/main.rs
+++ rust/dev/src/main.rs
@@ -11,4 +11,15 @@ fn main() {
 
     let ss = sum(10,vec![7,3,4,3,2]);
     println!("{}", ss);
+
+    life_time();
+}
+
+fn life_time() {
+    let n: i32;
+    {
+        let a = 5;
+        n = a;
+    }
+    println!("{}", n);
 }
