45ebfa8e0882a5b7d2075b2493e37b10a237e659
sRRRs <srrrs4510@gmail.com>
2024-07-13 23:18:28 +0900

    [add] pg vector query

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,7 +1,31 @@
 CREATE EXTENSION vector;
 
-CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3));
+CREATE TABLE vec_main_items (id bigserial PRIMARY KEY, embedding vector(3));
+CREATE TABLE vec_sub_items (id bigserial PRIMARY KEY, embedding vector(3));
 
-INSERT INTO items (embedding) VALUES ('[1,2,3]'), ('[4,5,6]');
+INSERT INTO vec_main_items (embedding)
+VALUES
+('[1,2,3]'),
+('[4,5,6]')
+;
 
-SELECT * FROM items ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
+INSERT INTO vec_sub_items (embedding)
+VALUES
+('[6,5,4]'),
+('[3,2,1]')
+;
+
+SELECT *
+FROM vec_main_items
+ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
+
+SELECT *
+FROM vec_sub_items
+ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
+
+SELECT
+  main.id AS vec_main_items,
+  sub.id AS vec_sub_items,
+  ROUND((main.embedding <-> sub.embedding)::numeric, 2) AS cosine_similarity
+FROM vec_main_items AS main, vec_sub_items AS sub
+WHERE main.id != sub.id;
