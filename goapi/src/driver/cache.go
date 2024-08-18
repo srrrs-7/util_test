9a3f49a04d1cbe7a0f0831e9a673e7aaee38ee37
sRRRs <srrrs4510@gmail.com>
2024-08-19 01:02:32 +0900

    [goapi] template implement

--- goapi/src/driver/cache.go
+++ goapi/src/driver/cache.go
@@ -1,14 +1,10 @@
 package driver
 
-import (
-	"fmt"
-
-	"github.com/redis/go-redis/v9"
-)
+import "github.com/redis/go-redis/v9"
 
 func NewCache(dsn string) *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprint(dsn),
+		Addr:     dsn,
 		Password: "",
 		DB:       0,
 	})
