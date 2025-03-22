8a56a0d5ab29a6e2da7045de56d6d922bd322113
sRRRs <srrrs4510@gmail.com>
2025-03-22 17:07:06 +0900

    [laravel] unittest

---
+++ laravel/tests/Unit/Models/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
