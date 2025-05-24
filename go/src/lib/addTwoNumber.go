2956e5cc36d91cd6b9a8278371885dba87e97fc7
sRRRs <srrrs4510@gmail.com>
2025-05-25 02:44:35 +0900

    leetcode

---
+++ go/src/lib/addTwoNumber.go
@@ -0,0 +1,27 @@
+package lib
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers(l1, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	curr := dummy
+	carry := 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		curr.Next = &ListNode{Val: sum % 10}
+		curr = curr.Next
+		carry = sum / 10
+	}
+	return dummy.Next
+}
